e6f8a61b9008a5c4587bdf9a6b1814cf4152ec0f
a037185a <a037185a@naver.com>
2021-11-21 12:56:12 +0900

    write

--- getopt,getopts.py
+++ getopt,getopts.py
@@ -3,3 +3,9 @@ getopt and getopts
 -> getopt명령어를 사용하는 이유
   1. 다양한 입력 값이 존재할 경우 개발자와 사용자의 편의를 위해서
   2. 스크립트를 보다 체계적으로 관리할 수 있다.
+ 
+-> getopt의 특징
+ 기본적으로 short, long옵션을 지원
+ 옵션 인수를 가질 경우, 문자를 사용하는 것은 getopts builtin 명령과 동일하다.
+short 옵션 지정은 -o, long 옵션 지정은 -l
+ getopt -o a:bc -> ':' 에 따라서 옵션 -a는 옵션 인수를 갖는다.
